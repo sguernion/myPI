de255580b7be7528e4fd8b855f2a940f7c50b1c3
Guernion Sylvain <gsylvain35@gmail.com>
2013-03-02 19:14:02 +0100

    postinstall

---
+++ postinstall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Mon script "post installation"  Raspbmc
+#
+
+# Mise a jour 
+#------------
+
+echo "Mise a jour de la liste des depots"
+apt-get update
+
+echo "Mise a jour du systeme"
+apt-get upgrade
+
+
+echo "Installation..."
+
+apt-get install git
+
+# bluetooth
+apt-get install bluetooth bluez
+sudo service bluetooth start
+#hcitool scan
+
+# Python Wiimote
+apt-get install python-cwiid
+
+# synthese vocal
+sudo apt-get install festival flite
+
+# sons
+sudo apt-get install alsa-utils mpg321
+
+#sudo modprobe snd-bcm2835
+#sudo amixer cset numid=3 1
+#amixer cset numid=3 -- 70%
+
+echo "Installation...done"
+
+# wget scripts
