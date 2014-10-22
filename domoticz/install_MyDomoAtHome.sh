4ad2f2357f57e411fe971979fd6730fb87a4a964
gsylvain35 <gsylvain35@gmail.com>
2014-10-22 16:07:08 +0200

    conf usb domoticz

--- domoticz/install_MyDomoAtHome.sh
+++ domoticz/install_MyDomoAtHome.sh
@@ -12,6 +12,8 @@ cp development.yml.def development.yml
 
 echo "##########"
 
+cp 98-usb-serial.rules /etc/udev/rules.d/98-usb-serial.rules
+
 # TODO Configuring the app
 # TODO add service
 # TODO Starting the application
